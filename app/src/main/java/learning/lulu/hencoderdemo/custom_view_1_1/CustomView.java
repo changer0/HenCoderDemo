a68b3125643a34a8767f15d71bab3e48e97bd821
changer0 <zhanglulu1007@foxmail.com>
2017-08-05 09:57:26 +0800

    init

---
+++ app/src/main/java/learning/lulu/hencoderdemo/custom_view_1_1/CustomView.java
@@ -0,0 +1,31 @@
+package learning.lulu.hencoderdemo.custom_view_1_1;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lulu on 17-8-5.
+ * 扔物线的第一课 / 自定义 View
+ */
+
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
